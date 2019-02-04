7e4e059c55a4cb515f7df1cde1194963d7c97c8f
fbeji <fbeji@unm.edu>
2019-02-04 16:58:13 -0700

    last version with monte carlo

--- src/edu/cnm/deepdive/craps/controller/Craps.java
+++ src/edu/cnm/deepdive/craps/controller/Craps.java
@@ -7,55 +7,68 @@ import java.util.Scanner;
 
 public class Craps {
 
-private Random rng;
-private Game game;// looking into our jar file
-
+  private Random rng;
+  private Game game;// looking into our jar file
 
 
   public static void main(String[] args) {
-
     Craps craps = new Craps();
-    craps.play(true);
-    try(Scanner scanner = new Scanner(System.in)) {
+    if (args.length > 0) {
+
+      int numTrials = Integer.parseInt(args[0]);
+      for (int i = 0; i < numTrials; i++) {
+        craps.play(false);
+      }
+ craps.displayTally();
+    } else {
+
+//    Craps craps = new Craps();
+//    craps.play(true);
+      try (Scanner scanner = new Scanner(System.in)) {
+        do {
+          craps.play(true);
+        } while (craps.playAgain(scanner));
+      }
+    }
+  }
+  public Craps() {
+      rng = new Random();
+      game = new Game(rng);
+
+    }
+
+    public void play ( boolean verbose){
+
+      game.reset();
+      State state = game.play();
+      if (verbose) {
+
+        for (int[] diceRoll : game.getRolls()) {
+          System.out.printf("%d:%d%n", diceRoll[0], diceRoll[1]);
+        }
+        System.out.printf("Outcome: %s%n", state);
+        displayTally();
+      }
+
+    }
+
+  private void displayTally() {
+    long wins = game.getWins();
+    long total = wins + game.getLosses();
+    double percentage = 100.0 * wins / total;
+    System.out.printf("Winning percentage = (%d/%d) = %.2f%n", wins, total, percentage);
+  }
+
+  private boolean playAgain (Scanner scanner){
+      System.out.print("Play again (y/n)?");
+      String input;
+
       do {
-        craps.play(true);
-      } while (craps.playAgain(scanner));
+        input = scanner.nextLine().trim().toLowerCase();
+      }
+      while (input.isEmpty());
+
+      return input.charAt(0) == 'y';
     }
   }
 
-  public Craps(){
-rng = new Random();
-game = new Game (rng);
-
-  }
-
-  public void play(boolean verbose){
-
-game.reset();
-State state = game.play();
-if (verbose){
-
-  for (int[] diceRoll : game.getRolls()){
-    System.out.printf("%d:%d%n", diceRoll[0],diceRoll[1]);
-  }
-  System.out.printf("Outcome: %s%n", state);
-  long wins = game.getWins();
-  long total = wins + game.getLosses();
-  double percentage = 100.0*wins/total;
-  System.out.printf("Winning percentage = (%d/%d) = %.2f%n",wins,total,percentage);
-}
-
-  }
-
-  private boolean playAgain(Scanner scanner){
-    System.out.print("Play again (y/n)?");
-    String input ;
-
-    do{
-      input = scanner.nextLine().trim().toLowerCase();
-    }
-    while (input.isEmpty());
-
-    return input.charAt(0)=='y';
-  }
-}
